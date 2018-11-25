70ebdf74f804e02e1a9efe31e404b5a0ae206511
mikeevans85 <baudecordier@gmail.com>
2018-11-25 10:03:02 -0600

    Added sortBy method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,12 +7,19 @@
       <p>Find out more information here.</p>
       <a class="btn btn-primary btn-lg" href="#/about" role="button">Learn more</a>
     </div> 
+    <div>
+      <button v-on:click="sortAttribute = 'name'"" class="btn btn-secondary">Sort by Name</button>
+      <button v-on:click="sortAttribute = 'price'"" class="btn btn-secondary">Sort by Price</button>
+    </div>
     <h1>Search Recipes:</h1>
-    <input type="text" v-model="wordFilter">
+    <input type="text" v-model="wordFilter" list="names">
+    <datalist id="names">
+      <option v-for="quilt in quilts">{{ quilt.name }}</option>
+    </datalist>
     <div class="container">
       <div class="row">
         <template id="QuiltCard">
-          <div v-for="quilt in filterBy(quilts, wordFilter, 'name')"" class="col">
+          <div v-for="quilt in orderBy(filterBy(quilts, wordFilter, 'name'), sortAttribute)" class="col">
             <div class="card-body" style="width: 18rem;">
               <img class="card-img-top" v-bind:src="quilt.images[0] ? quilt.images[0] : 'http://i2.wp.com/www.4motiondarlington.org/wp-content/uploads/2013/06/No-image-found.jpg?zoom=2&fit=300%2C300'" alt="Card image cap">
                 <div class="card-body">
@@ -45,6 +52,7 @@
         inputPrice: "",
         inputDescription: "",
         wordFilter: "",
+        sortAttribute: "",
         Errors: []
       };
     },
